2e2d5db2be861e4bdda99f664e10f1bb6f0b2efc
hoaint25 <ngthihoai251499@gmail.com>
2022-05-17 22:52:14 +0700

    load the packages

--- predictive_model.py
+++ predictive_model.py
@@ -5,6 +5,24 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objs as go
-
 from plotly import tools
-#from plotly.offline import download_plotlyjs
+from plotly.tools import FigureFactory as FF
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+#init_notebook_mode(connected=True)
+
+import gc
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from catboost import CatBoostClassifier
+from sklearn import svm
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
+import xgboost as xgb
+import os
+
+pd.set_option('display.max_columns', 100)
+
