c0798809ff78c52542ab396bf6f776728aaf7a0d
hoaint25 <ngthihoai251499@gmail.com>
2022-05-17 19:12:47 +0700

    coding

---
+++ predictive_model.py
@@ -0,0 +1,10 @@
+##load the packages
+import pandas as pd
+import numpy as np
+import matplotlib 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objs as go
+import plotly.fingure_factory as ff
+from plotly import tools
+#from plotly.offline import download_plotlyjs
